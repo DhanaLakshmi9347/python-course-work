7c4036d2c9e1f51c1cd9ac9135bd289578614118
DhanaLakshmi9347 <dhanalakshmigorrela798@gmail.com>
2026-07-24 07:26:27 +0530

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,32 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a= 20
+>>> b=10
+>>> a+b
+30
+>>> a-b
+10
+>>> a*b
+200
+>>> a/b
+2.0
+>>> a//b
+2
+>>> a%b
+0
+>>> 2**3
+8
+>>> 9/2
+4.5
+>>> 9%2
+1
+>>> 4**2
+16
+>>> a<b
+False
+>>> a>b
+True
+>>> a<=b
+False
+>>> a>=b
+True
